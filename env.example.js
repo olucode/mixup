001283484fe2609c59cb1e108c6832c2cbaa93bf
Olumide Falomo <olumightyfalomo@gmail.com>
2018-01-04 01:09:34 +0100

    Use Env To Determine API Url
    Set up an env file(env.js) that'd specify the environment, so that we
    can determine which API url to use in dev or production.
    This API url would be globally available to all components.
    
    Fix build issues; the previous UglifyJS plugin didn't support es6.

---
+++ env.example.js
@@ -0,0 +1,7 @@
+let APP_ENV='development';
+let MIXUP_API_URL='http://api.mixup.site';
+
+module.exports = {
+	APP_ENV,
+	MIXUP_API_URL
+}
--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,5 +1,9 @@
+envFile = require('../env')
+
 window._ = require('lodash');
 
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.apiUrl = envFile.APP_ENV == 'development' ? 'http://localhost:9090' : envFile.MIXUP_API_URL
